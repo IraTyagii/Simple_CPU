677601e902715f016cd21d129b5fbd95e94edd45
IraTyagii <iramybooksays@gmail.com>
2026-03-23 16:33:46 +0530

    Add register_file module for register operations

---
+++ RTL/register_file.v
@@ -0,0 +1,21 @@
+module register_file(input [1:0] read_addr1,
+     input [1:0] read_addr2,
+     input [1:0] write_addr,
+     input [7:0] write_data,
+     input write_enable, 
+     input clk,
+     
+     output [7:0] read_data1,
+     output [7:0] read_data2
+);
+reg [7:0] registers [0:3];
+
+assign read_data1 = registers[read_addr1];
+assign read_data2 = registers[read_addr2];
+
+always @(posedge clk) begin
+    if (write_enable) begin
+        registers[write_addr] <= write_data;
+    end
+end
+endmodule
